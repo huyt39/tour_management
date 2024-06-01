5ac4b9afcb397328e1d2893decd525b92b56ab1e
Huy Tuan <hotuanhuy1203@gmail.com>
2024-06-01 16:24:46 +0700

    pug

--- index.ts
+++ index.ts
@@ -3,8 +3,12 @@ import express, {Express, Request, Response} from "express";
 const app: Express = express();
 const port: number = 3000;
 
-app.get("/", (req: Request, res: Response)=>{
-    res.send("Trang chủ");
+
+app.set ("views", "./views");
+app.set ("view engine", "pug");
+
+app.get("/tours", (req: Request, res: Response)=>{
+    res.render("client/pages/tours/index.pug");
 });
 
 app.listen(port, ()=>{
