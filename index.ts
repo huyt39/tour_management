42aa9465bdf62ba86860c55328fbfd844cb9788a
Huy Tuan <hotuanhuy1203@gmail.com>
2024-06-01 16:15:23 +0700

    khoi tao index

---
+++ index.ts
@@ -0,0 +1,12 @@
+import express, {Express, Request, Response} from "express";
+
+const app: Express = express();
+const port: number = 3000;
+
+app.get("/", (req: Request, res: Response)=>{
+    res.send("Trang chủ");
+});
+
+app.listen(port, ()=>{
+    console.log(`App listening on port ${port}`);
+});
