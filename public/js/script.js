19dd3fb8921d8082dd0362c1b1d93a83c570e766
Huy Tuan <hotuanhuy1203@gmail.com>
2024-06-05 23:54:06 +0700

    slider

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,20 @@
-console.log("OK");
+// Slider Tour Detail 
+var imagesThumb = new Swiper(".imagesThumb", {
+    loop: true,
+    spaceBetween: 10,
+    slidesPerView: 3,
+    freeMode: true,
+    watchSlidesProgress: true,
+  });
+  var imagesMain = new Swiper(".imagesMain", {
+    loop: true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    thumbs: {  //lien ket main va thumb
+      swiper: imagesThumb,
+    },
+  });
+// End Slider Tour Detail 
